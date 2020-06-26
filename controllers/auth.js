aedf432c5d5e642e754a9fa3f9b1deef838885d8
AlfheimrTech <shiven@alfheimr.tech>
2020-06-27 00:23:29 +0530

    User Authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc      Register User
-// @route     GET /api/v1/auth/register
+// @route     POST /api/v1/auth/register
 // @access    Public
 
 exports.register = asyncHandler(async(req, res, next) => {
@@ -17,6 +17,40 @@ exports.register = asyncHandler(async(req, res, next) => {
         password,
         role,
     });
+
+    // Create Token (by running the method )
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token,
+    });
+});
+
+// @desc      Login User
+// @route     POST /api/v1/auth/login
+// @access    Public
+
+exports.login = asyncHandler(async(req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate email & password (since model validation isn't being used)
+    if (!email || !password)
+        return next(new ErrorResponse('Please provide an email & password', 400));
+
+    //Check for user (on the model)
+    const user = await User.findOne({ email }).select('+password');
+    console.log(user);
+    if (!user) {
+        return next(new ErrorResponse('Invalid Credentials', 401));
+    }
+
+    // Checking if password matches (on the returned obj)
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse('Please provide an email & password', 400));
+    }
 
     // Create Token (by running the method )
     const token = user.getSignedJwtToken();
--- models/User.js
+++ models/User.js
@@ -47,4 +47,9 @@ UserSchema.methods.getSignedJwtToken = function() {
     });
 };
 
+// Match user entered password to the hashed password in db
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model('User', UserSchema);
