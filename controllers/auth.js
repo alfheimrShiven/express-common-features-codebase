17202bc6949028661c8b13d11918c1dd203c874f
AlfheimrTech <shiven@alfheimr.tech>
2020-07-03 22:21:55 +0530

    Update Logged in User's Profile

--- controllers/auth.js
+++ controllers/auth.js
@@ -143,6 +143,48 @@ exports.resetPassword = asyncHandler(async(req, res, next) => {
     });
 });
 
+// @desc      Update user details
+// @route     Put /api/v1/auth/updatedetails
+// @access    Private
+exports.updateDetails = asyncHandler(async(req, res, next) => {
+    const fieldstoUpdate = {
+        name: req.body.name,
+        email: req.body.email,
+    };
+
+    const user = await User.findByIdAndUpdate(req.user.id, fieldstoUpdate, {
+        new: true,
+        runValidators: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        data: user,
+    });
+});
+
+// @desc      Update Password
+// @route     PUT /api/v1/auth/updatepassword
+// @access    Private
+exports.updatePassword = asyncHandler(async(req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    // Check current password
+    if (!(await user.matchPassword(req.body.currentPassword))) {
+        return next(new ErrorResponse('Password is incorrect', 401));
+    }
+
+    user.password = req.body.newPassword;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
+
+    res.status(200).json({
+        success: true,
+        data: user,
+    });
+});
+
 // Helper Function: Generate token from User model, create cookie and send response
 
 const sendTokenResponse = (user, statusCode, res) => {
